b41acd8bbb88b628090550ab2261dc7cf403c7b5
Johnny Gu <johnny@wiredcraft.com>
2022-04-18 22:40:47 +0800

    chore: print input param

--- Sources/FigmaTokenGenerator/main.swift
+++ Sources/FigmaTokenGenerator/main.swift
@@ -1,1 +1,16 @@
-print("Hello, world!")
+import ArgumentParser
+import Foundation
+
+struct FigmaTokenGenerator: ParsableCommand {
+    @Argument(help: "input json file path")
+    var inputPath: String
+
+    @Argument(help: "output swift file path")
+    var outputPath: String
+
+    func run() {
+        print(inputPath)
+    }
+}
+
+FigmaTokenGenerator.main()
