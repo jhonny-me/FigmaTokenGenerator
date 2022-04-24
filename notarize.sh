bc4f622e40207bf495ea8b192bd5b7dccfbcc28b
Johnny Gu <johnny@wiredcraft.com>
2022-04-24 10:22:01 +0800

    chore: notarize zip file

---
+++ notarize.sh
@@ -0,0 +1,1 @@
+xcrun altool --notarize-app --primary-bundle-id "com.johnny.figmatokengenerator" --asc-provider "Team ID" -u "iphone@wiredcraft.com" -p "APP SPECIFIEC PWD" -t osx -f FigmaTokenGenerator-0.1.5.zip
